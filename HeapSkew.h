7d0fccbbc0f0b9526169b571c31287c606400796
slcaneer42 <slcaneer42@students.tntech.edu>
2017-04-20 09:50:19 -0500

    Compiling but not functioning correctly
    This currently outputs 2 nodes to the screen.

--- HeapSkew.h
+++ HeapSkew.h
@@ -53,43 +53,62 @@ bool HeapSkew<T>::heapIsEmpty()
 template < class T >
 BinaryTree<T>* HeapSkew<T>::merge(BinaryTree<T>* left, BinaryTree<T>* right)
 {
-  //DO THIS
+        //DO THIS
 
+        if(left->isEmpty())
+        {
+                delete left;
+                return right;
+        }
+        if(right->isEmpty())
+        {
+                delete right;
+                return left;
+        }
+        if((left->getRootItem()) < (right->getRootItem()))
+        {
+            bt =  merge(right, left); //should this be assigned to somethin
+        }
 
+        BinaryTree<T>* LL = left->detachLeftSubtree();
+        BinaryTree<T>* LR = left->detachRightSubtree();
 
+        left->attachLeftSubtree(LL);
 
-
-
-
-
-
-
-
-
-
-
+        if(LR->isEmpty())
+        {
+                left->attachLeftSubtree(right);
+                return left;
+        }
+        else
+        {
+                right = merge(LR, right);
+                //left->attachLeftSubtree(right);
+                return left;
+        }
 }
 
 template < class T >
 void HeapSkew<T>::heapInsert(T* item)
 {
-   //DO THIS (calls merge, should be short)
-
-
-
-
+        //DO THIS (calls merge, should be short)
+        BinaryTree<T>* right = new BinaryTree<T>(item);
+        BinaryTree<T>* left = bt;
+        bt = merge(left, right);
+        sze++;
 }
 
 template < class T >
 T* HeapSkew<T>::heapRemove()
 {
-   //DO THIS (calls merge, should be short)
-
-
-
-
-
-
+        //DO THIS (calls merge, should be short)
+        T* root = bt->getRootItem();
+        BinaryTree<T>* left = bt->detachLeftSubtree();
+        BinaryTree<T>* right = bt->detachRightSubtree();
+        delete bt;
+        bt = merge(left, right);
+        sze--;
+        return root;
 }
 
 template < class T >
